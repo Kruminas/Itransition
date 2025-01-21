362e131d1a441a76e98d6286a76b552c48a17410
Dominykas <59967266+Kruminas@users.noreply.github.com>
2025-01-22 01:03:27 +0200

    .

--- 2025-01-18/DiceGame/Utils.js
+++
@@ -1,6 +0,0 @@
-function getUserInput(prompt) {
-  process.stdout.write(prompt);
-  return new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString().trim())));
-}
-
-module.exports = { getUserInput };
